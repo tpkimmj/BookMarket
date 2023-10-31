bfeb55565077c0b316783fc0ca8a8c202ee4fd85
RufusVein <148760176+RufusVein@users.noreply.github.com>
2023-10-31 09:42:00 +0900

    고객센터1

---
+++ src/main/java/com/book/client/web/ClientController.java
@@ -0,0 +1,40 @@
+package com.book.client.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.book.member.dto.MemberDTO;
+import com.book.member.service.MemberService;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Controller
+public class ClientController {
+	@Autowired
+	MemberService memberService;
+	
+	@GetMapping("/ClientCenter") //고객센터
+	public String ClientCenter(HttpServletRequest request, HttpServletResponse response, MemberDTO mdto, Model model) {
+		String page = null;
+		
+		page = "Main";
+		
+		model.addAttribute("page", page);
+		model.addAttribute("contentsJsp", "custom/ClientCenter");
+		return page;
+	}
+	@RequestMapping("/AdminClientCenter") //관리자 고객센터
+	public String AdminClientCenter(HttpServletRequest request, HttpServletResponse response, MemberDTO mdto, Model model) {
+		String page = null;
+		
+		page = "Main";
+		
+		model.addAttribute("page", page);
+		model.addAttribute("contentsJsp", "admin/AdminClientCenter");
+		return page;
+	}
+}
--- src/main/java/com/book/member/web/MemberController.java
+++ src/main/java/com/book/member/web/MemberController.java
@@ -172,4 +172,5 @@ public class MemberController {
 		model.addAttribute("contentsJsp", "/MemList");
 		return page;
 	}
+	
 }
