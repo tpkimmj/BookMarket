3710173f31af3f2f0e506106ba7ef812930ab217
tpkimmj <tpkimmj92@naver.com>
2023-11-10 09:41:38 +0900

    클라이언트 컨트롤러 줄맞춤/불필요 조스 정리/주석
    클라이언트수정폼에서 목록가기 버튼 추가

--- src/main/java/com/book/client/web/ClientController.java
+++ src/main/java/com/book/client/web/ClientController.java
@@ -41,35 +41,34 @@ public class ClientController {
 		return page;
 	}	
 	
-	@RequestMapping("/ClientCenter")
+	@RequestMapping("/ClientCenter") //공지사항 리스트
 	 public String ClientCenter(HttpServletRequest request, 
 			   HttpServletResponse response, 
 			   Model model, PageDTO pageDto,
 			   ClientDTO cdto) {
-	      Map<String, Object> reSet = clientService.getClients(cdto);
-	      HttpSession session = request.getSession();
-	      String contentsJsp = null;
-	      String page = null;
-	      MemberDTO mdto = (MemberDTO) session.getAttribute("ssKey");
-	      
-	      if(mdto!=null && mdto.getM_role().equals("admin")) {
-	         page = "admin/Main";
-	         contentsJsp = "/ClientCenter";
-	      }else {//고객, 방문자
-	         page = "Main";
-	         contentsJsp = "custom/ClientCenter";
-	      }
-	       @SuppressWarnings("unchecked")
-		List<ClientDTO> cList = (List<ClientDTO>) reSet.get("cList"); 
-//	       logger.info("controller==>"+cList.size());
+		
+	    Map<String, Object> reSet = clientService.getClients(cdto);
+	    HttpSession session = request.getSession();
+	    String contentsJsp = null;
+	    String page = null;
+	    MemberDTO mdto = (MemberDTO) session.getAttribute("ssKey");
+	    if(mdto!=null && mdto.getM_role().equals("admin")) {
+	    	page = "admin/Main";
+	    	contentsJsp = "/ClientCenter";
+	    }else {//고객, 방문자
+	        page = "Main";
+	        contentsJsp = "custom/ClientCenter";
+	    }
+	    @SuppressWarnings("unchecked")
+	    List<ClientDTO> cList = (List<ClientDTO>) reSet.get("cList"); 
 	       
-	      session.setAttribute("ssKey", mdto);
-	      model.addAttribute("pageDto", reSet.get("pageDto"));
-	      model.addAttribute("totCnt", reSet.get("totCnt"));
-	      model.addAttribute("cList", cList);
-	      model.addAttribute("contentsJsp", contentsJsp);
-	      return page;
-	   }
+	    session.setAttribute("ssKey", mdto);
+	    model.addAttribute("pageDto", reSet.get("pageDto"));
+	    model.addAttribute("totCnt", reSet.get("totCnt"));
+	    model.addAttribute("cList", cList);
+	    model.addAttribute("contentsJsp", contentsJsp);
+	    return page;
+	}
 	
 	@RequestMapping("noticeProc")// 공지사항 등록
 	public String noticeProc(HttpServletRequest request, HttpServletResponse response,
@@ -84,89 +83,90 @@ public class ClientController {
 	    mdto = (MemberDTO) session.getAttribute("ssKey");
 	    logger.info("====>"+cdto);
 		String msg = null;
-	    String url = null;
-		if(mdto!=null && mdto.getM_role().equals("admin")){
-				switch (flag) {
-	            case "insert":{
-	            	clientService.generateClient(cdto);
-	               page = "redirect:/ClientCenter";
-	               break;
-	            }
+		String url = null;
+	    if(mdto!=null && mdto.getM_role().equals("admin")){
+			switch (flag) {
+				case "insert":{
+					clientService.generateClient(cdto);
+					page = "redirect:/ClientCenter";
+					break;
+				}
 	            case "update":{
-	               //수정용
-	               page = "MsgPage";
-	               logger.info("cdto==>"+cdto);
-	               int r = clientService.updateProc(cdto);
-	               if(r>0) msg = "수정이 완료되었습니다.";
-	               else msg = "수정을 실패했습니다."; 
-	               
-	               break;
+					//수정용
+					page = "MsgPage";
+					logger.info("cdto==>"+cdto);
+					int r = clientService.updateProc(cdto);
+					if(r>0) msg = "수정이 완료되었습니다.";
+					else msg = "수정을 실패했습니다."; 
+					break;
 	            }
 	            case "delete":{
-	                //삭제용
-	                page = "MsgPage";
+					//삭제용
+					page = "MsgPage";
 	                int r = clientService.deleteProc(cdto);
 	                logger.info("cdto==>"+cdto);
 	                if(r>0) msg = "삭제가 완료되었습니다.";
 	                else msg = "삭제을 실패했습니다."; 
 	                break;
-	             }
 	            }
-	            url = "ClientCenter";
-	            if(url!=null) model.addAttribute("url", url);
-	            if(msg!=null) model.addAttribute("msg", msg);
-	      }else {
-	         page = "Main";
-	         contentsJsp = "./custom/ClientCenter";
-	      }
-		  session.setAttribute("ssKey", mdto);
-	      model.addAttribute("cdto", cdto);
-	      model.addAttribute("cList", reSet.get("ClientCenter"));
-	      model.addAttribute("contentsJsp", contentsJsp);
+			}
+			url = "ClientCenter";
+			if(url!=null) model.addAttribute("url", url);
+			if(msg!=null) model.addAttribute("msg", msg);
+	    	}else {
+	    		page = "Main";
+	    		contentsJsp = "./custom/ClientCenter";
+	    	}
+	    
+	    session.setAttribute("ssKey", mdto);
+	    model.addAttribute("cdto", cdto);
+	    model.addAttribute("cList", reSet.get("ClientCenter"));
+	    model.addAttribute("contentsJsp", contentsJsp);
 		return page;
 	}
-	@RequestMapping("/noticeDetail")
-	   public String noticeDetail(HttpServletRequest request, HttpServletResponse response, 
+	@RequestMapping("/noticeDetail") //게시된 공지사항 보기
+	public String noticeDetail(HttpServletRequest request, HttpServletResponse response, 
 			   ClientDTO cdto, Model model, PageDTO pageDto) {
-	      HttpSession session = request.getSession();
-	      String contentsJsp = null;
-	      String page = null;
-	      MemberDTO mdto = (MemberDTO) session.getAttribute("ssKey");
-	      if(mdto!=null && mdto.getM_role().equals("admin")) {
-	    	  ClientDTO notice = clientService.getClient(cdto);
-	            model.addAttribute("cdto", notice);
-	            page = "admin/Main";
-	            contentsJsp = "./NoticeUpForm";
-	      }else {
-	         //고객용에서도 조회가 되어야 할 듯함(조회수 증가)
-	    	  Map<String, Object> reSet = clientService.getClients(cdto);
-	    	   ClientDTO dto = clientService.getClient(cdto);
-	    	  model.addAttribute("cdto", dto);
-	         page = "Main";
-	         contentsJsp = "./custom/Notice";
-	      }
-	      session.setAttribute("ssKey", mdto);
-	      model.addAttribute("contentsJsp", contentsJsp);
+		HttpSession session = request.getSession();
+		String contentsJsp = null;
+		String page = null;
+		MemberDTO mdto = (MemberDTO) session.getAttribute("ssKey");
+		if(mdto!=null && mdto.getM_role().equals("admin")) {
+			ClientDTO notice = clientService.getClient(cdto);
+			model.addAttribute("cdto", notice);
+			page = "admin/Main";
+			contentsJsp = "./NoticeUpForm";
+		}else {
+			//고객용에서만 조회수가 증가
+			clientService.getClients(cdto);
+			
+			ClientDTO dto = clientService.getClient(cdto);
+			model.addAttribute("cdto", dto);
+			page = "Main";
+			contentsJsp = "./custom/Notice";
+		}
+		session.setAttribute("ssKey", mdto);
+		model.addAttribute("contentsJsp", contentsJsp);
 	      
-	      return page;
-	   }
-	@RequestMapping("/noticeUpForm")
-	   public String noticeUpForm(HttpServletRequest request, HttpServletResponse response, ClientDTO cdto, Model model, PageDTO pageDto) {
-	      HttpSession session = request.getSession();
-	      String contentsJsp = null;
-	      String page = null;
-	      MemberDTO mdto = (MemberDTO) session.getAttribute("ssKey");
+		return page;
+	}
+	@RequestMapping("/noticeUpForm") //공지사항 수정
+	public String noticeUpForm(HttpServletRequest request, HttpServletResponse response, ClientDTO cdto, Model model, PageDTO pageDto) {
+		HttpSession session = request.getSession();
+		String contentsJsp = null;
+		String page = null;
+		MemberDTO mdto = (MemberDTO) session.getAttribute("ssKey");
 	      
-	      if(mdto!=null && mdto.getM_role().equals("admin")) {
-	         page = "admin/Main";
-	         contentsJsp = "./NoticeUpForm";
-	      }else {
-	         page = "Main";
-	         contentsJsp = "./custom/ClientCenter";
-	      }
-	      session.setAttribute("ssKey", mdto);
-	      session.setAttribute("notice", cdto);
-	      model.addAttribute("contentsJsp", contentsJsp);
-	      return page;
-	   }
+		if(mdto!=null && mdto.getM_role().equals("admin")) {
+			page = "admin/Main";
+			contentsJsp = "./NoticeUpForm";
+		}else {
+			page = "Main";
+			contentsJsp = "./custom/ClientCenter";
+	    }
+		session.setAttribute("ssKey", mdto);
+		session.setAttribute("notice", cdto);
+		model.addAttribute("contentsJsp", contentsJsp);
+		return page;
+	}
 }
